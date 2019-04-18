615346010f26485de9092f61a9d026e37b14e555
claireparker <claireparker@users.noreply.github.com>
2019-04-18 16:43:36 +0100

    :eyeglasses: Add autoprefixing through Hugo Pipes

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    plugins: {
+        autoprefixer: {
+            browsers: [
+                "last 2 versions",
+                "not dead"
+            ]
+        }
+    },
+}
